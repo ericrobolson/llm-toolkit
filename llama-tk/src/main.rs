303809d639c6ff1fa176a77676bec28ca2dbfdc0
eric.olson <eric.rob.olson@gmail.com>
2025-04-18 07:48:53 -0700

    Got basic app setup to run and download + start server

--- llama-tk/src/main.rs
+++ llama-tk/src/main.rs
@@ -1,11 +1,18 @@
 use rusqlite::Connection;
-use std::path::PathBuf;
+use std::{path::PathBuf, process::Command};
 
 fn main() {
     let dir: PathBuf = ".llama-tk/".into();
     std::fs::create_dir_all(&dir).unwrap();
 
-    let db_path = dir.clone().join("llama-db.sqlite");
+    setup_db(&dir);
+    setup_repo(&dir);
+    source_models(&dir);
+    start_server(&dir);
+}
+
+fn setup_db(path: &PathBuf) {
+    let db_path = path.join("llama-db.sqlite");
     let db = Connection::open(db_path).unwrap();
 
     db.execute(
@@ -32,5 +39,120 @@ fn main() {
 
     for table in names {
         println!("{}", table);
+    }
+}
+
+fn setup_repo(path: &PathBuf) {
+    let repo_path = path.join("llama.cpp");
+    if !repo_path.exists() {
+        println!("Cloning repo...");
+        let command = "git clone https://github.com/ggerganov/llama.cpp.git";
+
+        let command = Command::new("git")
+            .args(&["clone", "https://github.com/ggerganov/llama.cpp.git"])
+            .current_dir(&path)
+            .output()
+            .unwrap();
+
+        if command.status.success() {
+            println!("Repo cloned successfully");
+        } else {
+            panic!(
+                "Failed to clone repo: {}",
+                String::from_utf8(command.stderr).unwrap()
+            );
+        }
+
+        let command = "cd llama.cpp && cmake -B build && cmake --build build --config Release";
+
+        let command = Command::new("cmake")
+            .args(&["-B", "build"])
+            .current_dir(&repo_path)
+            .output()
+            .unwrap();
+
+        if command.status.success() {
+            println!("CMake build successful");
+        } else {
+            panic!(
+                "CMake build failed: {}",
+                String::from_utf8(command.stderr).unwrap()
+            );
+        }
+
+        println!("Building...");
+        let command = Command::new("cmake")
+            .args(&["--build", "build", "--config", "Release"])
+            .current_dir(&repo_path)
+            .output()
+            .unwrap();
+
+        if command.status.success() {
+            println!("CMake build successful");
+        } else {
+            panic!(
+                "CMake build failed: {}",
+                String::from_utf8(command.stderr).unwrap()
+            );
+        }
+    } else {
+        println!("Repo exists")
+    }
+}
+
+fn start_server(path: &PathBuf) {
+    println!("Starting server...");
+    println!("http://127.0.0.1:8080");
+    let repo_path = path.join("llama.cpp");
+
+    let models_path = path.join("models");
+    let model = models_path.join("openhermes-2.5-mistral-7b.Q5_K_S.gguf");
+    let bin_path = repo_path.join("build/bin/llama-server");
+    let command = Command::new(bin_path)
+        .args(&["--port", "8080"])
+        .args(&["--model", model.to_str().unwrap()])
+        .output()
+        .unwrap();
+
+    if command.status.success() {
+        println!("Server started");
+    } else {
+        panic!(
+            "Server failed to start: {}",
+            String::from_utf8(command.stderr).unwrap()
+        );
+    }
+}
+
+fn source_models(path: &PathBuf) {
+    let models_path = path.join("models");
+    std::fs::create_dir_all(&models_path).unwrap();
+    let model = "openhermes-2.5-mistral-7b.Q5_K_S.gguf";
+    let url = "https://huggingface.co/TheBloke/OpenHermes-2.5-Mistral-7B-GGUF/resolve/main/openhermes-2.5-mistral-7b.Q5_K_S.gguf";
+    let model_path = models_path.join(model);
+    if !model_path.exists() {
+        /*
+                URL=https://huggingface.co/TheBloke/OpenHermes-2.5-Mistral-7B-GGUF/resolve/main/openhermes-2.5-mistral-7b.Q5_K_S.gguf
+
+        mkdir -p models
+        curl -L $URL > models/openhermes-2.5-mistral-7b.Q5_K_S.gguf
+                 */
+
+        println!("Downloading model...");
+        let command = Command::new("curl")
+            .args(&["-L", url])
+            .args(&["-O"])
+            .current_dir(&models_path)
+            .output()
+            .unwrap();
+
+        if command.status.success() {
+            println!("Model downloaded");
+        } else {
+            panic!(
+                "Model failed to download: {}",
+                String::from_utf8(command.stderr).unwrap()
+            );
+        }
     }
 }
